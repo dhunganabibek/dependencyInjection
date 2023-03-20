8f1eea9d710d5df60b20870b156223dda5e20fcb
dhunganabibek <Dhunganabibek2016@gmail.com>
2023-03-20 09:11:39 -0500

    autowired added

--- src/main/java/com/bibekdhungana/Charger.java
+++ src/main/java/com/bibekdhungana/Charger.java
@@ -4,50 +4,50 @@ import java.util.Objects;
 
 public class Charger {
 
-    private int id;
-    private String name;
-    private String color;
+    // private int id;
+    // private String name;
+    // private String color;
 
     public Charger() {
     }
 
-    public Charger(int id, String name, String color) {
-        this.id = id;
-        this.name = name;
-        this.color = color;
-    }
+    // public Charger(int id, String name, String color) {
+    // this.id = id;
+    // this.name = name;
+    // this.color = color;
+    // }
 
-    public int getId() {
-        return this.id;
-    }
+    // public int getId() {
+    // return this.id;
+    // }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+    // public void setId(int id) {
+    // this.id = id;
+    // }
 
-    public String getName() {
-        return this.name;
-    }
+    // public String getName() {
+    // return this.name;
+    // }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    // public void setName(String name) {
+    // this.name = name;
+    // }
 
-    public String getColor() {
-        return this.color;
-    }
+    // public String getColor() {
+    // return this.color;
+    // }
 
-    public void setColor(String color) {
-        this.color = color;
-    }
+    // public void setColor(String color) {
+    // this.color = color;
+    // }
 
-    @Override
-    public String toString() {
-        return "{" +
-                " id='" + getId() + "'" +
-                ", name='" + getName() + "'" +
-                ", color='" + getColor() + "'" +
-                "}";
-    }
+    // @Override
+    // public String toString() {
+    // return "{" +
+    // " id='" + getId() + "'" +
+    // ", name='" + getName() + "'" +
+    // ", color='" + getColor() + "'" +
+    // "}";
+    // }
 
 }
--- src/main/java/com/bibekdhungana/Laptop.java
+++ src/main/java/com/bibekdhungana/Laptop.java
@@ -7,9 +7,9 @@ import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Laptop {
 
-    private int id;
-    private String name;
-    private String color;
+    // private int id;
+    // private String name;
+    // private String color;
 
     @Autowired
     @Qualifier("chargerTwo")
@@ -18,53 +18,53 @@ public class Laptop {
     public Laptop() {
     }
 
-    public Laptop(int id, String name, String color, Charger charger) {
-        this.id = id;
-        this.name = name;
-        this.color = color;
-        this.charger = charger;
-    }
+    // public Laptop(int id, String name, String color, Charger charger) {
+    // this.id = id;
+    // this.name = name;
+    // this.color = color;
+    // this.charger = charger;
+    // }
 
-    public int getId() {
-        return this.id;
-    }
+    // public int getId() {
+    // return this.id;
+    // }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+    // public void setId(int id) {
+    // this.id = id;
+    // }
 
-    public String getName() {
-        return this.name;
-    }
+    // public String getName() {
+    // return this.name;
+    // }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    // public void setName(String name) {
+    // this.name = name;
+    // }
 
-    public String getColor() {
-        return this.color;
-    }
+    // public String getColor() {
+    // return this.color;
+    // }
 
-    public void setColor(String color) {
-        this.color = color;
-    }
+    // public void setColor(String color) {
+    // this.color = color;
+    // }
 
-    public Charger getCharger() {
-        return this.charger;
-    }
+    // public Charger getCharger() {
+    // return this.charger;
+    // }
 
-    public void setCharger(Charger charger) {
-        this.charger = charger;
-    }
+    // public void setCharger(Charger charger) {
+    // this.charger = charger;
+    // }
 
-    @Override
-    public String toString() {
-        return "{" +
-                " id='" + getId() + "'" +
-                ", name='" + getName() + "'" +
-                ", color='" + getColor() + "'" +
-                ", charger='" + getCharger() + "'" +
-                "}";
-    }
+    // @Override
+    // public String toString() {
+    // return "{" +
+    // " id='" + getId() + "'" +
+    // ", name='" + getName() + "'" +
+    // ", color='" + getColor() + "'" +
+    // ", charger='" + getCharger() + "'" +
+    // "}";
+    // }
 
 }
